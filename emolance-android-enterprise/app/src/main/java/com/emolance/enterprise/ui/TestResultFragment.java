30ffb1b500621bb5b58fdedc26000b55e3e7fa21
Erick Rivera <erickrivera2012@gmail.com>
2018-01-18 20:12:24 -0800

    back button working #60
    also adjusted button position and added scrollview fadeout

--- emolance-android-enterprise/app/src/main/java/com/emolance/enterprise/ui/TestResultFragment.java
+++ emolance-android-enterprise/app/src/main/java/com/emolance/enterprise/ui/TestResultFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.emolance.enterprise.R;
 
@@ -20,6 +21,8 @@ public class TestResultFragment extends Fragment {
 
     @InjectView(R.id.send_btn)
     Button sendButton;
+    @InjectView(R.id.backButtonResultScreen)
+    ImageButton backButton;
 
     public TestResultFragment() {
         // Required empty public constructor
@@ -31,6 +34,12 @@ public class TestResultFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_test_result, container, false);
         ButterKnife.inject(this,rootView);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().onBackPressed();
+            }
+        });
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
